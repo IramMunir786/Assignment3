588040cbbf460833cd549b0a86b23f1b9c50ec5a
IramMunir786 <bcsf17a510@pucit.edu.pk>
2020-12-19 15:12:36 +0500

    Calculator App Added

--- app/src/main/java/com/example/assignment3/Calculator.java
+++ app/src/main/java/com/example/assignment3/Calculator.java
@@ -2,13 +2,32 @@ package com.example.assignment3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.os.Handler;
+
+
 
 public class Calculator extends AppCompatActivity {
 
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getApplicationContext(),CalculatorMainScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
     }
 }
